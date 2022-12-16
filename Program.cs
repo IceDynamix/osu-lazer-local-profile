d90e29982c2a12d77f7c520689bb3afaa44ea52b
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-12-16 11:44:53 +0100

    Scoping

--- Program.cs
+++ Program.cs
@@ -12,51 +12,13 @@ using osu.Game.Tests.Beatmaps;
 using osu.Game.Utils;
 using Realms;
 
-void PrintScore(ScoreInfo scoreInfo, double pp, int i1)
-{
-    string TimeAgoString(DateTimeOffset dateTime)
-    {
-        var span = DateTimeOffset.Now.Subtract(dateTime);
-        if (span.TotalDays < 0) return "time traveler";
-
-        if (span.Hours < 1) return "now";
-        if (span.Days < 1) return $"{span.Hours}h ago";
-        if (span.Days < 7) return $"{span.Days}d ago";
-        if (span.Days < 30) return $"{span.Days / 7}w ago";
-        if (span.Days < 365) return $"{span.Days / 12}mo ago";
-
-        return $"{span.Days / 365}y ago";
-    }
-
-    ConsoleColor RowColor(ScoreInfo score)
-    {
-        var span = DateTimeOffset.Now.Subtract(score.Date);
-        if (span.Hours < 1) return ConsoleColor.Magenta;
-        if (span.Days < 1) return ConsoleColor.Red;
-        if (span.Days < 7) return ConsoleColor.Yellow;
-        if (span.Days < 30) return ConsoleColor.Green;
-        return ConsoleColor.White;
-    }
-
-    var ppString = $"{pp:f0}pp";
-    var accString = scoreInfo.Accuracy.FormatAccuracy();
-    var timeString = TimeAgoString(scoreInfo.Date);
-    var modString = scoreInfo.Mods.Length > 0 ? "+" + String.Join(',', scoreInfo.Mods.Select(m => m.Acronym)) : "";
-
-    Console.ForegroundColor = RowColor(scoreInfo);
-    Console.WriteLine($"{i1 + 1, 5} | {ppString, 6}, {accString, 6} | {timeString, 10} | {scoreInfo.BeatmapInfo.StarRating:f1}* | {scoreInfo.BeatmapInfo} {modString}");
-    Console.ResetColor();
-}
-
 if (args.Length == 0) throw new Exception("Missing osu directory argument");
 if (args.Length == 1) throw new Exception("Missing ruleset argument");
 
 var osuDir = args[0];
-var mode = args[1];
-
 if (!Directory.Exists(osuDir) || !Directory.Exists(osuDir + "/files")) throw new Exception("osu directory was invalid");
 
-Ruleset ruleset = mode switch
+Ruleset ruleset = args[1] switch
 {
     "osu" => new OsuRuleset(),
     "taiko" => new TaikoRuleset(),
@@ -73,29 +35,31 @@ var realm = Realm.GetInstance(new RealmConfiguration(osuDir + @"/client.realm")
 Console.WriteLine("Created database instance");
 
 var personalBests = new Dictionary<string, ScoreInfo>();
-foreach (var score in realm.All<ScoreInfo>().Filter("Rank > -1"))
 {
-    if (score.Ruleset.ShortName != mode) continue;
-    if (score.BeatmapInfo.Status != BeatmapOnlineStatus.Ranked) continue;
-    if (score.Mods.Any(m => m is ModClassic)) continue;
-    if (personalBests.ContainsKey(score.Hash))
-        if (score.TotalScore < personalBests[score.Hash].TotalScore)
-            continue;
+    foreach (var score in realm.All<ScoreInfo>().Filter("Rank > -1"))
+    {
+        if (score.Ruleset.ShortName != ruleset.ShortName) continue;
+        if (score.BeatmapInfo.Status != BeatmapOnlineStatus.Ranked) continue;
+        if (score.Mods.Any(m => m is ModClassic)) continue;
+        if (personalBests.ContainsKey(score.Hash))
+            if (score.TotalScore < personalBests[score.Hash].TotalScore)
+                continue;
 
-    personalBests.Add(score.Hash, score);
+        personalBests.Add(score.Hash, score);
+    }
+    Console.WriteLine("Computed personal bests");
 }
 
-Console.WriteLine("Computed personal bests");
-
-WorkingBeatmap GetWorkingBeatmap(string hash)
-{
-    using (var stream = File.OpenRead($"{osuDir}/files/{hash[0]}/{hash[0..2]}/{hash}"))
-    using (var reader = new LineBufferedReader(stream))
-        return new TestWorkingBeatmap(Decoder.GetDecoder<Beatmap>(reader).Decode(reader));
-}
 
 var scores = new List<(ScoreInfo, double)>();
 {
+    WorkingBeatmap GetWorkingBeatmap(string hash)
+    {
+        using (var stream = File.OpenRead($"{osuDir}/files/{hash[0]}/{hash[0..2]}/{hash}"))
+        using (var reader = new LineBufferedReader(stream))
+            return new TestWorkingBeatmap(Decoder.GetDecoder<Beatmap>(reader).Decode(reader));
+    }
+
     var i = 0;
     var count = personalBests.Values.Count();
     foreach (var score in personalBests.Values)
@@ -122,29 +86,68 @@ var scores = new List<(ScoreInfo, double)>();
         }
     }
 
+    Console.WriteLine();
+
     scores = scores.OrderByDescending(s => s.Item2).ToList();
 }
 
-Console.WriteLine();
 
-var weightedPp = 0d;
-var weightedAcc = 0d;
-
-for (int i = 0; i < scores.Count(); i++)
 {
-    var (score, pp) = scores[i];
-
-    var weight = Math.Pow(0.95, i);
-    weightedPp += pp * weight / 20;
-    weightedAcc += score.Accuracy * weight / 20;
-
-    if (i < 25)
+    void PrintScore(ScoreInfo scoreInfo, double pp, int i1)
     {
-        PrintScore(score, pp, i);
-    }
-}
+        string TimeAgoString(DateTimeOffset dateTime)
+        {
+            var span = DateTimeOffset.Now.Subtract(dateTime);
+            if (span.TotalDays < 0) return "time traveler";
 
-Console.WriteLine($"{scores.Count()} filtered scores, {weightedPp:f2} avg pp, {weightedPp * 20:f2} total pp, {weightedAcc * 100:f2}% avg acc");
+            if (span.Hours < 1) return "now";
+            if (span.Days < 1) return $"{span.Hours}h ago";
+            if (span.Days < 7) return $"{span.Days}d ago";
+            if (span.Days < 30) return $"{span.Days / 7}w ago";
+            if (span.Days < 365) return $"{span.Days / 12}mo ago";
+
+            return $"{span.Days / 365}y ago";
+        }
+
+        ConsoleColor RowColor(ScoreInfo score)
+        {
+            var span = DateTimeOffset.Now.Subtract(score.Date);
+            if (span.Hours < 1) return ConsoleColor.Magenta;
+            if (span.Days < 1) return ConsoleColor.Red;
+            if (span.Days < 7) return ConsoleColor.Yellow;
+            if (span.Days < 30) return ConsoleColor.Green;
+            return ConsoleColor.White;
+        }
+
+        var ppString = $"{pp:f0}pp";
+        var accString = scoreInfo.Accuracy.FormatAccuracy();
+        var timeString = TimeAgoString(scoreInfo.Date);
+        var modString = scoreInfo.Mods.Length > 0 ? "+" + String.Join(',', scoreInfo.Mods.Select(m => m.Acronym)) : "";
+
+        Console.ForegroundColor = RowColor(scoreInfo);
+        Console.WriteLine($"{i1 + 1, 5} | {ppString, 6}, {accString, 6} | {timeString, 10} | {scoreInfo.BeatmapInfo.StarRating:f1}* | {scoreInfo.BeatmapInfo} {modString}");
+        Console.ResetColor();
+    }
+
+    var weightedPp = 0d;
+    var weightedAcc = 0d;
+
+    for (int i = 0; i < scores.Count(); i++)
+    {
+        var (score, pp) = scores[i];
+
+        var weight = Math.Pow(0.95, i);
+        weightedPp += pp * weight / 20;
+        weightedAcc += score.Accuracy * weight / 20;
+
+        if (i < 25)
+        {
+            PrintScore(score, pp, i);
+        }
+    }
+
+    Console.WriteLine($"{scores.Count()} filtered scores, {weightedPp:f2} avg pp, {weightedPp * 20:f2} total pp, {weightedAcc * 100:f2}% avg acc");
+}
 
 Console.Write("Press any key to close");
 Console.Read();
