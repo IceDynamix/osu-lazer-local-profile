ba6ca69cc6ddcd0037307c30bf13720b7a160f44
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-12-16 08:46:19 +0100

    Read scores from database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using osu.Game.Scoring;
+using Realms;
+
+var realm = Realm.GetInstance(new RealmConfiguration(@"D:\Games\osu-lazer\client - Copy.realm")
+{
+    SchemaVersion = 25 // relates to RealmAccess.schema_version
+});
+
+Console.WriteLine($"{realm.All<ScoreInfo>().Count()} scores in database");
